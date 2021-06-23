e1106d899e47f57f03a9b12d579516d5c076478d
Dilshard <mrdilshard@gmail.com>
2021-06-23 16:26:28 +0530

    My initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        Marks student = new Marks();
+
+        for (int i=1; i<=2; i++){
+            student.mk1 = scan.nextInt();
+            student.mk2 = scan.nextInt();
+            student.tot();
+        }
+    }
+}
---
+++ src/com/company/Marks.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Marks {
+
+
+    int mk1;
+    int mk2;
+
+    public void tot(){
+        int tot = mk1+mk2;
+        System.out.println("\nTotal: "+tot);
+    }
+
+}
